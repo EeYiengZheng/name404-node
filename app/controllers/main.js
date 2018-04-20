f83ba6ffcd052626a06e7f8afcb7da9810c86ade
Ee Yieng Zheng <eeyiengz@hotmail.com>
2018-04-20 14:34:58 -0700

    small changes

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -4,19 +4,27 @@
 module.exports.index = function (req, res, next) {
     res.render('index', {
         title: 'CS 157B - Assignment 7',
-        welcome: ''
+        welcome: 'Click on links below to access the required REST API calls',
+        api: {
+            list: '../orderlist/',
+            show: '../orderlist/',
+            new: '../neworder/',
+            edit: '../editorder/',
+            create: '../neworder/',
+            update: '../editorder/',
+            delete: '../deleteorder/'
+        }
     });
 };
 
-module.exports.get_deleteorder = function(req, res) {
-    var o_no = req.params.orderno;
-    res.render('deleteorder', { "orderno" : o_no} );
+module.exports.get_deleteorder = function (req, res) {
+    var o_no = req.params.orderno;
+    res.render('deleteorder', {"orderno": o_no});
 };
 /*
  * GET new user form
  */
-module.exports.get_neworder = function(req, res) 
-{
-    res.render('neworder', { "title": 'Add New Order' });
+module.exports.get_neworder = function (req, res) {
+    res.render('neworder', {"title": 'Add New Order'});
 };
 
